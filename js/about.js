72a1d324158e449f0571aae808d9b93e50e99825
MayPhyoKyaw <MayPhyoKyaw@users.noreply.github.com>
2021-02-02 22:19:57 +0630

    merge with ksp file

--- js/about.js
+++ js/about.js
@@ -1,13 +1,24 @@
-$(".fa-plus-square-payment-method").click(function () {
-  var men = document.getElementById("payment_method");
-  // $(this).removeClass('fa-plus-square')
-  // $(this).addClass('fa-minus-square')
-  men.classList.toggle('hidden');
+$(".fa-plus-square-company-profile").click(function () {
+  var companyProfile = document.getElementById("company_profile");
+  companyProfile.classList.toggle('hidden');
 })
 
-$(".fa-plus-square-money-back").click(function () {
-  var women = document.getElementById("money_back");
-  // $(this).removeClass('fa-plus-square')
-  // $(this).addClass('fa-minus-square')
-  women.classList.toggle('hidden');
+$(".fa-plus-square-payment-method").click(function () {
+  var payment = document.getElementById("payment_method");
+  payment.classList.toggle('hidden');
+})
+
+$(".fa-plus-square-price").click(function () {
+  var price = document.getElementById("price");
+  price.classList.toggle('hidden');
+})
+
+$(".fa-plus-square-delivery-method").click(function () {
+  var delivery = document.getElementById("delivery_method");
+  delivery.classList.toggle('hidden');
+})
+
+$(".fa-plus-square-privacy").click(function () {
+  var privacy = document.getElementById("privacy");
+  privacy.classList.toggle('hidden');
 })
