06258f5b9f8968327a308126d13e39a4ea587dc4
Paing Kaung Sett <kaungsettpaing@haj-emp.co.jp>
2021-02-15 13:33:48 +0900

    update credit payment js file

--- js/creditPayment.js
+++ js/creditPayment.js
@@ -1,23 +1,3 @@
-$(document).ready(function () {
-  $(".card-submit").click(function () {
-    $(".card-name").append($(".cards").val())
-    $(".card-number").append($(".card-no").val())
-    $(".card-expired-date").append($(".card-expired-month").val() + '/' + $(".card-expired-year").val())
-    $(".card-user-name").append($(".card-username").val())
-  })
-  $("#card_confirmation").on("hidden.bs.modal", function () {
-    $(".card-name").text('')
-    $(".card-number").text('')
-    $(".card-expired-date").text('')
-    $(".card-user-name").text('')
-  })
-  $(".cancel").click(function () {
-    $(".card-name").text('')
-    $(".card-number").text('')
-    $(".card-expired-date").text('')
-    $(".card-user-name").text('')
-  })
-})
 function goBack() {
   window.history.back();
 }
