7b05862fa9b582bde804ff758d094f21487b2799
MayPhyoKyaw <MayPhyoKyaw@users.noreply.github.com>
2021-03-04 22:35:34 +0630

    last

--- creditPaymentConfirm.php
+++ creditPaymentConfirm.php
@@ -381,7 +381,7 @@
                                         <!-- <span class="credit-conf-description">消費税 : <?php //echo $_SESSION['consumption_tax']; ?></span> <br/> -->
                                         <span class="credit-conf-description">配送料 : <?php echo "￥" . $_SESSION['delivery_fee']; ?></span> <br/>
                                         <span class="credit-conf-description">割引率 : <?php echo "￥-" . $_SESSION['discount-rate']; ?></span> <br/>
-                                        <span class="credit-conf-description">合計金額 : <?php echo "￥" . number_format($total_amount); ?></span>
+                                        <span class="credit-conf-description">合計金額 : <?php echo "￥" . $total_amount; ?></span>
                                         <?php 
                                             $cName = null; 
                                             $cPhone = null;
